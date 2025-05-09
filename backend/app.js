95837e25f5974ef95a1960545600cc11529c6f8f
Rustam Kudratov <kudratov.rustik@gmail.com>
2025-05-09 22:09:41 +0300

    added dockerfile

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
@@ -15,7 +16,7 @@ app.use(cookieParser());
 
 app.use("/api", routes);
 
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
     res.sendFile(path.resolve("..", "frontend", "build", "index.html"));
 });
 
